19aa967a849cd7cdef09ab33f00e9df54fcfbf92
Luis Carlin <luiscarlin@gmail.com>
2017-02-11 19:29:13 -0500

    Initilize grid with ramdon pattern

--- game-of-life.js
+++ game-of-life.js
@@ -1,26 +1,42 @@
-function game(dom) {
+function game(dom, width, height) {
 
-    var ctx = dom.getContext("2d"), 
-        cells = [];
+    var ctxt = dom.getContext("2d")
+    ctxt.canvas.width = width
+    ctxt.canvas.height = height
 
-    init();
-    run();
+    var grid = fillRandom(createNewGrid(width, height))
+    draw(grid, ctxt)
+    run(grid, ctxt)
 
-    function init() { 
-        console.log("initialize the grid");
-        draw();
+    function fillRandom(arr) {
+        for (var i = 0; i < arr.length; i++) { 
+            for (var j = 0; j < arr[i].length; j++) { 
+                arr[i][j] = Math.round(Math.random())
+            }
+        }
+        return arr
     }
 
-    function run() { 
-        console.log("New Generation");
-        draw();
-
-        setTimeout(function() { run(); }, 3000);
+    function createNewGrid(numCols, numRows) {
+        var arr = new Array(numCols)
+        for (var i = 0; i < numCols; i++) { 
+          arr[i] = new Array(numRows)
+        }
+        return arr
     }
 
-    function draw() { 
-        console.log("drawing grid")
+    function draw(arr, ctxt) {
+        ctxt.clearRect(0, 0, ctxt.height, ctxt.width)
+
+        for (var i = 0; i < arr.length; i++) { 
+            for (var j = 0; j < arr[i].length; j++) { 
+                if(arr[i][j] === 1) { 
+                    // alive
+                    ctxt.fillRect(i, j, 1, 1)
+                }
+            }
+        }
     }
 }
 
-game(document.getElementById("canvas"));
+game(document.getElementById("canvas"), 400, 400)
